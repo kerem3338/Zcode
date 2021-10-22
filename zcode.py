20e23493346a8760232e167b93301ef9d06cca70
kerem3338 <73388608+kerem3338@users.noreply.github.com>
2021-10-22 21:32:29 +0300

    Add files via upload

--- zcode.py
+++ zcode.py
@@ -10,73 +10,85 @@ import os
 import getpass
 import sys
 
-def clear():
-	if os.name == "nt":
-		os.system("cls")
-	else:
-		os.system("clear")
-clear()
-print(f"Zcode - Kullanıcı: {getpass.getuser()}")
 
-#Pencere
-pencere=tk.Tk()
+def clear():
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+clear()
+print(f"Zcode v1.0 - Kullanıcı: {getpass.getuser()}")
+
+# Pencere
+pencere = tk.Tk()
 pencere.title("Zcode")
 pencere.geometry("500x500")
 
+
+def dosyakapat(e=None):
+    ent.delete(1.0, "end")
+
+def kaydet(e=None):
+    dosya_uzantilari = [('Python Dosyası', '*.py'),
+                        ('Bütün Dosyalar', '*.*')]
+    file = fd.asksaveasfile(filetypes=dosya_uzantilari, mode="w", defaultextension=".py")
+    if file is None:
+        return
+    text = str(ent.get(1.0, END))
+    file.write(text)
+    file.close()
 def hakkinda():
-	tkinter.messagebox.showinfo("Hakkında", "Zcode v1.0")
+    tkinter.messagebox.showinfo("Hakkında", "Zcode v1.0")
+
+
 def ac():
-	filename = fd.askopenfilename()
-	if filename == "":
-		pass
-	else:
-		with open(filename, "r", encoding="utf8") as file:
-			file=file.read()
-		ent.delete(1.0, "end")
-		ent.insert(1.0, file)
-	
-def run():
-	clear()
-	ent_veri = ent.get(1.0, tk.END+"-1c")
-	if ent_veri == "":
-		tkinter.messagebox.showinfo("Hata", "Boş Kod çalıştırılamaz")
-		status['text'] = "Hata"
-	else:
-            
-                try:
-                        exec(ent_veri)
-                        status['text'] = "Başarılı"
-                except BaseException as error:
-                        status['text'] = "Hata"
-                        tkinter.messagebox.showinfo("Hata", error)
-                else:
-                        pass
-	"""if ent_veri == "":
-		tkinter.messagebox.showinfo("Hata", "Boş Kod çalıştırılamaz")
-		status['text'] = "Hata"
-	else:
-		status['text'] = "Komut çalıştırılıyor"
-		try:
-			exec(ent_veri)
-		except BaseException as error:
-			tkinter.messagebox.showinfo("Hata", error)
-			status['text'] = "Hata"
-		else:
-			tkinter.messagebox.showinfo("Output", exec(ent_veri))
-        """		
-#Menu
+    filename = fd.askopenfilename()
+    if filename == "":
+        pass
+    else:
+        with open(filename, "r", encoding="utf8") as file:
+            file = file.read()
+        ent.delete(1.0, "end")
+        ent.insert(1.0, file)
+
+
+def run(e=None):
+    clear()
+    ent_veri = ent.get(1.0, tk.END+"-1c")
+    if ent_veri == "":
+        tkinter.messagebox.showinfo("Hata", "Boş Kod çalıştırılamaz")
+        status['text'] = "Hata"
+    else:
+
+        try:
+            exec(ent_veri)
+            status['text'] = "Başarılı"
+        except BaseException as error:
+            status['text'] = "Hata"
+            tkinter.messagebox.showinfo("Hata", error)
+        else:
+            pass
+
+
+
+# Menu
 menu = tk.Menu(pencere)
 filemenu = tk.Menu(menu)
 yardim = tk.Menu(menu)
-#Dosya menüsü
+ayarlar = tk.Menu(menu)
+# Dosya menüsü
 filemenu.add_command(label="Dosya Aç", command=ac)
-#Yardım menüsü
+filemenu.add_command(label="Dosya Kaydet", command=kaydet)
+# Yardım menüsü
 yardim.add_command(label="Hakkında", command=hakkinda)
-
+#Menülerin eklenmesi
 menu.add_cascade(label="Dosya", menu=filemenu)
 menu.add_cascade(label="Yardım", menu=yardim)
 
-#kod
+
+# kod
 ent = tk.Text(pencere, width=500)
 ent.pack(padx=50)
 font = tkfont.Font(font=ent['font'])
@@ -96,24 +108,29 @@ cdg.tagdefs['DEFINITION'] = {'foreground': '#007F7F', 'background': '#FFFFFF'}
 
 ip.Percolator(ent).insertfilter(cdg)
 
-#Tab
+# Tab
 tab_size = font.measure('    ')
 ent.config(tabs=tab_size)
 
-#Statü
+# Statü
 status = tk.Label(text="Başlatılmadı")
 status.pack()
 
-#Çalıştır
+# Çalıştır
 buton = tk.Button(text="Çalıştır", command=run)
 buton.place(x=12, y=4400)
 buton.pack()
 
-#Config
+# Config
 pencere.config(menu=menu)
-#Döngü
+
+#Klavye kısayolları
+pencere.bind("<F5>", run)
+pencere.bind("<Control-w>", dosyakapat)
+pencere.bind("<Control-s>", kaydet)
+# Döngü
 try:
-	pencere.mainloop()
+    pencere.mainloop()
 except:
-	clear()
-	sys.exit()
+    clear()
+    sys.exit()
